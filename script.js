5aeac4b1d89e1cea232b4b48b30537113d9f6ee8
Claudia Petty <claudialove32@gmail.com>
2020-04-17 14:00:26 -0400

    beginning scaffold of html, js file with function list

--- script.js
+++ script.js
@@ -2,16 +2,42 @@
 >>JS<<
 functions needed
 
+
+- function to make search button render state information
+	- function to render search list
+	- function to make search list items into a button
+	- function to make that button render state information 
+
+
+
+        
+
+        
+
+        // Here we construct our URL
+        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+       
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then (function(response){
+          console.log(queryURL);
+          console.log(response);
+          $("#movie-view").text(JSON.stringify(response))
+          
+        })
+        
+      });
+
+
 	- function to call on ajax for US
 	- function to call on US info
 		
 	- function to call on ajax for states
 	- function to display states info
 
-	- function to make search button render state information
-	- function to render search list
-	- function to make search list items into a button
-	- function to make that button render state information
 
 	- function to localStorage setItem
 	- function to localStorage getItem
@@ -21,6 +47,47 @@ functions needed
 	-function to 
 
 */
+
+
+
+
+
+
+    // This .on("click") function will trigger the AJAX Call
+    $("#searchForm").on("click", function(event) {
+
+        // Preventing the submit button from trying to submit the form
+        // We're optionally using a form so the user may hit Enter to search instead of clicking the button
+        event.preventDefault();
+
+        // Here we grab the text from the input box
+        var movie = $("#SearchCity").val();
+
+        
+        // Here we construct our URL
+        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+       
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then (function(response){
+          console.log(queryURL);
+          console.log(response);
+          $("#SearchList").text(JSON.stringify(response))
+          
+        })
+        
+      });
+
+
+
+
+
+
+
+
+
 
 $.ajax({
     //url: "https://api.covid19api.com/live/country/united-states/status/confirmed",
