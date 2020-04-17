445c955ed76721fd3f89b6bd7d9afac3a528f055
Kouros <kamousavi@gmail.com>
2020-04-17 10:25:28 -0400

    initial push of html and js files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+$.ajax({
+    //url: "https://api.covid19api.com/live/country/united-states/status/confirmed",
+    url: "https://covidtracking.com/api/states/daily?state=VA&date=20200415",
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+    //console.log(response[0].Province);
+    //console.log(response[0].Confirmed);
+});
