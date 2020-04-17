2088ed5889a4a28781e8fde4e7389d6edee8e288
Kouros <kamousavi@gmail.com>
2020-04-17 14:04:20 -0400

    added date yesterday functionality to API

--- script.js
+++ script.js
@@ -22,13 +22,21 @@ functions needed
 
 */
 
+var MyDate = new Date();
+var myDateString;
+
+MyDate.setDate(MyDate.getDate() - 1);
+
+myDateString = MyDate.getFullYear() + ('0' + (MyDate.getMonth()+1)).slice(-2) + ('0' + MyDate.getDate()).slice(-2);
+
+console.log(myDateString);
+
+
+
 $.ajax({
     //url: "https://api.covid19api.com/live/country/united-states/status/confirmed",
-    url: "https://covidtracking.com/api/states/daily?state=VA&date=20200415",
+    url: "https://covidtracking.com/api/states/daily?state=VA&date=" + myDateString,
     method: "GET"
 }).then(function (response) {
     console.log(response);
-    //console.log(response[0].Province);
-    //console.log(response[0].Confirmed);
-    console.log("Hello this is Kouros");
 });
